b77c3f9514153105253b73230be2225cdcb17aaf
elchun02 <103269534+elchun02@users.noreply.github.com>
2022-04-29 09:47:25 -0700

    updating markdownparse

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,6 +12,9 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()-1) {
+            if (!markdown.contains("[")) {
+                return toReturn;
+            }
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
