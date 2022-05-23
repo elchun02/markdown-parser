73e83cbc5778470f91a1bc6281f59c149b78c84d
elchun02 <103269534+elchun02@users.noreply.github.com>
2022-05-22 21:30:37 -0700

    lab report 4

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -31,5 +31,31 @@ public class MarkdownParseTest {
         List<String> expected = new ArrayList<>(List.of());
         assertEquals(expected, MarkdownParse.getLinks(content));
     }
+
+    @Test
+    public void testSnippet1() throws Exception {
+        Path nameOfFile = Path.of("./snippet1.md");
+        String content = Files.readString(nameOfFile);
+        List<String> expected = new ArrayList<>(List.of("url.com", "google.com", "google.com", "ucsd.edu"));
+        assertEquals(expected, MarkdownParse.getLinks(content));
+    }
+
+    @Test
+    public void testSnippet2() throws Exception {
+        Path nameOfFile = Path.of("./snippet2.md");
+        String content = Files.readString(nameOfFile);
+        List<String> expected = new ArrayList<>(List.of("a.com", "a.com(())", "example.com"));
+        assertEquals(expected, MarkdownParse.getLinks(content));
+    }
+
+    @Test
+    public void testSnippet3() throws Exception {
+        Path nameOfFile = Path.of("./snippet3.md");
+        String content = Files.readString(nameOfFile);
+        List<String> expected = new ArrayList<>(List.of("https://www.twitter.com", "https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule", "https://cse.ucsd.edu/"));
+        assertEquals(expected, MarkdownParse.getLinks(content));
+    }
+
+
 }
 
